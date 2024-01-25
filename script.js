bf642fe4253f4f57607dfc2c4efad910d1372011
frankiebelize <66229510+frankiebelize@users.noreply.github.com>
2024-01-25 04:09:26 -0600

    finished home page

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ["Frontend Developer","Backend Developer","SQL Developer","C++ Developer" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
